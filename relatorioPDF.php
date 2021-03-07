ff090695b50e0b2fc54b012fc1442ca2fe2c16ca
CarolAguera <77307756+CarolAguera@users.noreply.github.com>
2021-03-07 10:50:52 -0300

    instalando mPDF

---
+++ relatorioPDF.php
@@ -0,0 +1,31 @@
+<?php
+include("mpdf60/mpdf.php");
+require_once __DIR__ . '../../vendor/autoload.php';
+
+$html = "
+ <fieldset>
+ <h1>Comprovante de Pagamento</h1>
+ <p class='center sub-titulo'>
+ Nº <strong>0001</strong> - 
+ VALOR <strong>R$ 700,00</strong>
+ </p>
+ <p>Recebi(emos) de <strong>Ebrahim Paula Leite</strong></p>
+ <p>a quantia de <strong>Setecentos Reais</strong></p>
+ <p>Correspondente a <strong>Serviços prestados ..<strong></p>
+ <p>e para clareza firmo(amos) o presente.</p>
+ <p class='direita'>Umuarama, 07 de Março de 2021</p>
+ <p>Assinatura ......................................................................................................................................</p>
+ <p>Nome <strong>Carolina Aguera</strong> CPF/CNPJ: <strong>106.280.659-06</strong></p>
+ <p>Endereço <strong>Rua Treze de Maio, 1829 - Jardim Colibri, Umuarama - Paraná</strong></p>
+ </fieldset>
+ ";
+
+
+$mpdf = new \Mpdf\mPDF();
+$mpdf->SetDisplayMode('fullpage');
+$css = file_get_contents("css/style.css");
+$mpdf->WriteHTML($css, 1);
+$mpdf->WriteHTML($html);
+$mpdf->Output();
+
+exit;
