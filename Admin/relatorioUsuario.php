cc76827bb2631b5ffb3adea36dd88ae7e338912a
CarolAguera <77307756+CarolAguera@users.noreply.github.com>
2021-03-09 17:36:04 -0300

    finalizando relatório de usuario

--- Admin/relatorioUsuario.php
+++ Admin/relatorioUsuario.php
@@ -3,56 +3,48 @@ require_once("../verificaSessao.php");
 include("../mpdf60/mpdf.php");
 require_once __DIR__ . '../../../vendor/autoload.php';
 
-$conexao = mysqli_connect('127.0.0.1', 'root', '', 'tcc');
-if (!$conexao) {
-	die("Falha na conexao: " . mysqli_connect_error());
-} else {
-	//echo "Conexao realizada com sucesso";
+function getFooter(){  
+	$retorno = "<table class=\"tbl_footer\" width=\"1000\">  
+			<tr> 
+			  <td align=\"left\"><a href='malito:carolaguerabr@gmail.com'>carolaguerabr@gmail.com</a></td>  
+			  <td align=\"right\">Página: {PAGENO}</td>  
+			</tr>  
+		  </table>";  
+	return $retorno;  
+  }  
+
+
+function getTabela()
+{
+	$retorno = "";
+	$retorno .= "<h2 style=\"text-align:center\">Depósito Brasil</h2>";
+	$retorno .= "<h4 style=\"text-align:center\">Relatório de Usuário</h4>";
+	$retorno .= "<table border='1' width='1000' align='center'>  
+		 <tr class='header'>  
+			 <th>ID</th>
+		 	<th>Nome Completo</th>
+		 	<th>E-mail</th>
+		 </tr>";
+
+	$conexao = mysqli_connect('127.0.0.1', 'root', '', 'tcc');
+	$sql = "select * from usuario  ";
+	$usuarios = mysqli_query($conexao, $sql);
+	mysqli_close($conexao);
+	while ($data = mysqli_fetch_array($usuarios)) {
+		$retorno .= "<tr class=\"zebra\">";
+		$retorno .=	"<td class='destaque'>{$data['id']} </td>";
+		$retorno .= "<td>{$data['nome_completo']} </td>";
+		$retorno .= "<td>{$data['email']} </td>";
+	}
+	$retorno .= "</table>";
+	return $retorno;
 }
 
-$conexao = mysqli_connect('127.0.0.1', 'root', '', 'tcc');
-$sql = "select * from usuario  ";
-$usuarios = mysqli_query($conexao, $sql);
-
-$html = "
-<html>
-
-<body>
-<h1>Relatório do Usuário</h1>
-	<table>
-		<thead>
-			<tr>
-				<th>ID</th>
-				<th>Status</th>
-				<th>Usuário</th>
-				<th>Ações</th>
-			</tr>
-		</thead>
-		<tbody>
-			";
-
-while ($data = mysqli_fetch_array($usuarios)) {
-	$opa = (string) $data['id'];
-	"
-			<tr>
-				<td> " .  $opa  . " </td>
-				<td> " .  $data['nome_completo'] . " </td>
-				<td> " . $data['email']  . "</td>
-			</tr>
-				";
-}
-" 
-
-		</tbody>
-	</table>
-</body>
-
-</html>
-";
 $mpdf = new \Mpdf\mPDF();
 $mpdf->SetDisplayMode('fullpage');
 $css = file_get_contents("../css/estilo.css");
 $mpdf->WriteHTML($css, 1);
-$mpdf->WriteHTML($html);
+$mpdf->SetHTMLFooter(getFooter());  
+$mpdf->WriteHTML(getTabela());
 $mpdf->Output();
 exit;
