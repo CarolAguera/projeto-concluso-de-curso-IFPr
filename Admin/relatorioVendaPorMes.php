5cc9c6f25a92f970ed5fc7936fdd0217a169f513
CarolAguera <77307756+CarolAguera@users.noreply.github.com>
2021-03-31 10:17:50 -0300

    desenvolvi relatorio de vendas por mes

--- Admin/relatorioVendaPorMes.php
+++ Admin/relatorioVendaPorMes.php
@@ -21,29 +21,36 @@ function getTabela()
 {
   $retorno = "";
   $retorno = "<img class='imagem' src=\"../img/dpbrasillogo.png\"> ";
-  $retorno .= "<h4 style=\"text-align:center\">Relatório de Vendas por Mês</h4>";
+  $retorno .= "<h4 style=\"text-align:center\">Relatório de Vendas do Mês Atual</h4>";
   $retorno .= "<table border='1' width='1000' align='center'>  
 		 <tr class='header'>  
          <th>ID</th>s
          <th>Usuário</th>
          <th>Valor Total</th>
          <th>Desconto</th>
-         <th>Data e Hora</th>
+         <th>Valor Total da Venda</th>
+         <th>Data Da Venda</th>
 		 </tr>";
 
   $conexao = mysqli_connect('127.0.0.1', 'root', '', 'tcc');
-  $sql = "SELECT venda.*, usuario.nome_completo as nome_usuario FROM venda
-           INNER JOIN usuario ON venda.Usuario_id = usuario.id";
 
+  $data_incio = mktime(0, 0, 0, date('m'), 1, date('Y'));
+  $data_fim = mktime(0, 0, 0, date('m') + 1, 1, date('Y'));
+
+
+  $sql = "SELECT venda.*, usuario.nome_completo as nome_usuario FROM venda
+           INNER JOIN usuario ON venda.Usuario_id = usuario.id
+           WHERE data_hora BETWEEN ". "'". date('Y-m-d', $data_incio). "'" . " AND " . "'" . date('Y-m-d', $data_fim) . "'". "";         
   $vendas = mysqli_query($conexao, $sql);
   mysqli_close($conexao);
   while ($data = mysqli_fetch_array($vendas)) {
     $retorno .= "<tr class=\"zebra\">";
     $retorno .= "<td class='destaque'>{$data['id']} </td>";
     $retorno .= "<td>{$data['nome_usuario']} </td>";
-    $retorno .= "<td>" . number_format($data['valorTotal'], 2, ',', ' ') . "</td>";
+    $retorno .= "<td>" . number_format($data['valorTotal'], 2, ',', '.') . "</td>";
     $retorno .= "<td> " . number_format($data['desconto'], 2, ',', '') . "</td>";
-    $retorno .= "<td>". new DateTime($data) date('d/m/Y H:i:s',$data['data_hora']) ."</td>";
+    $retorno .= "<td>" . number_format($data['valorTotal'] - $data['desconto'], 2, ',', '.') . "</td>";
+    $retorno .= "<td>" . (new DateTime($data['data_hora']))->format('d/m/Y') . "</td>";
     // $retorno .= "<td>". date('d/m/Y H:i:s',$data['data_hora']) ."</td>";
   }
   $retorno .= "</table>";
@@ -58,9 +65,3 @@ $mpdf->SetHTMLFooter(getFooter());
 $mpdf->WriteHTML(getTabela());
 $mpdf->Output();
 exit;
-
-
-//array(16) { [0]=> string(1) "9" ["id"]=> string(1) "9" [1]=> string(1) "1" 
-//["status"]=> string(1) "1" [2]=> string(19) "2021-03-25 13:29:00"
-//[" data_hora"]=> string(19) "2021-03-25 13:29:00" [3]=> string(2) "30" 
-//["valorTotal"]=> string(2) "30" [4]=> string(1) "0" ["desconto"]=> string(1) "0" [5]=> string(1) "1" ["Usuario_id"]=> string(1) "1" [6]=> string(1) "2" ["Cliente_id"]=> string(1) "2" [7]=> string(15) "Carolina Aguera" ["nome_usuario"]=> string(15) "Carolina Aguera" }
